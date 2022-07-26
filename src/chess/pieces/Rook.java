360e7b163a32a17aa183c84cb124a69d41b24d5f
Kayky Freitas <kayky_defreitas@hotmail.com>
2022-07-26 15:33:15 -0300

    Fix - Better treatment to promotion

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -5,8 +5,6 @@ import boardGame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-import java.util.Scanner;
-
 public class Rook extends ChessPiece {
 
     public Rook(Board board, Color color) {
